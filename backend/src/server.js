70e1f55f62040d8cd09754da971f6e4b260173db
amans-k <srktv6195@gmail.com>
2025-11-01 13:20:27 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
+
+dotenv.config();
+
+const app = express();
+ console.log(ENV.PORT);
+  console.log(ENV.DB_URL);
+
+app.get ("/health", (req, res) => {
+    res.status(200).json({message: "Succes from api"})
+})
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is running on port 3000")
+})
