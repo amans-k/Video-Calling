be665a1f34bb32c90fbe3a59df5ac7aa61fece1b
amans-k <srktv6195@gmail.com>
2025-11-03 13:33:09 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,34 @@
 import express from "express";
 import path from "path";
-import dotenv from "dotenv";
-import { ENV } from "./lib/env.js";
-import { clearScreenDown } from "readline";
-import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
-dotenv.config();
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
+
 const __dirname = path.resolve();
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ message: "api is up and running" });
-});
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ message: "this is the books end point" });
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // ✅ Fixed wildcard route
-  app.get(/.*/, (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
