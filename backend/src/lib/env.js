50950564113d19607321a5349d45d419057d76e2
amans-k <srktv6195@gmail.com>
2025-11-04 11:20:16 +0530

    server side auth implemented

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,11 +4,11 @@ dotenv.config(); // ✅ Loads all environment variables from .env
 
 export const ENV = {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL, // ✅ Added this line
+  DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_URL: process.env.CLIENT_URL,
   INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
   INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
   STREAM_API_KEY: process.env.STREAM_API_KEY,
-  STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET, // ✅ Fixed name here
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRouttes.js";
+
+
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -19,6 +22,8 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
